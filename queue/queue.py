c3be9f8a5f74dff3c0d53d8218231c4c6d25548b
Logan Negley <logannegley@gmail.com>
2020-06-19 18:24:16 -0500

    finished linkedList with queue getting errors

--- queue/queue.py
+++ queue/queue.py
@@ -17,20 +17,39 @@ from singly_linked_list import LinkedList
 from singly_linked_list import LinkedList
 
 # Implementing with list
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+    
+#     def __len__(self):
+#         self.size
+
+#     def enqueue(self, value):
+#         self.storage.insert(-1, value)
+
+#     def dequeue(self):
+#         if len(self.storage) == 0:
+#             return None
+#         else:
+#             self.size -= 1
+#             return self.storage.pop(-1)
+
+# Linked_List
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
     
     def __len__(self):
         self.size
 
     def enqueue(self, value):
-        self.storage.insert(-1, value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if len(self.storage) == 0:
             return None
         else:
             self.size -= 1
-            return self.storage.pop(0)
+            return self.storage.remove_head()
