674a736581b4b11e4774c922b1121386af586fbc
Logan Negley <logannegley@gmail.com>
2020-06-25 16:21:56 -0500

    got contains and get_max passing

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -35,11 +35,34 @@ class BSTNode:
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        # if target is equal to the root value than its in the tree
+        if target == self.value:
+            return True
+        # if the root value is more than the target we check the left side
+        if self.value >= target:
+            # check to see if left side has a value or is none
+            if self.left == None:
+                return False
+
+            elif self.left.contains(target):
+                return True
+        
+        if self.value <= target:
+            if self.right == None:
+                return False
+
+            elif self.right.contains(target):
+                return True 
 
     # Return the maximum value found in the tree
     def get_max(self):
-        pass
+        if self.right == None:
+            return self.value
+        maxNode = False
+        if self.right:
+            maxNode = self.right.get_max()
+            return maxNode
+            
 
     # Call the function `fn` on the value of each node
     def for_each(self, fn):
